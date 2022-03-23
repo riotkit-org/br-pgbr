415f64ef660e6c01f9b9d180854d938420a25826
blackandred <22807686+blackandred@users.noreply.github.com>
2022-03-23 22:34:22 +0100

    ci: Improving pipelines

--- ci.mk
+++ ci.mk
@@ -15,9 +15,9 @@ dockerfile:
 	cat release.Dockerfile >> .build/Dockerfile
 
 ci_release_snapshot:
-	docker build . -f .build/Dockerfile --build-arg POSTGRES_VERSION=${POSTGRES_VERSION} --build-arg GO_VERSION=${GO_VERSION} -t ghcr.io/riotkit-org/pgbr:latest
-	docker push ghcr.io/riotkit-org/pgbr:latest
+	docker build . -f .build/Dockerfile --build-arg POSTGRES_VERSION=${POSTGRES_VERSION} --build-arg GO_VERSION=${GO_VERSION} -t ghcr.io/riotkit-org/pgbr:latest-pg${POSTGRES_VERSION}
+	docker push ghcr.io/riotkit-org/pgbr:latest-pg${POSTGRES_VERSION}
 
 ci_release:
-	docker tag ghcr.io/riotkit-org/pgbr:latest ghcr.io/riotkit-org/pgbr:$${GITHUB_REF##*/}
-	docker push ghcr.io/riotkit-org/pgbr:$${GITHUB_REF##*/}
+	docker tag ghcr.io/riotkit-org/pgbr:latest-pg${POSTGRES_VERSION} ghcr.io/riotkit-org/pgbr:$${GITHUB_REF##*/}-pg${POSTGRES_VERSION}
+	docker push ghcr.io/riotkit-org/pgbr:$${GITHUB_REF##*/}-pg${POSTGRES_VERSION}
