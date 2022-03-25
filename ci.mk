21a0cabab9d4de0aacb0edac6fba0c7958be1fd0
blackandred <22807686+blackandred@users.noreply.github.com>
2022-03-25 07:59:10 +0100

    ci: Try a matrix release

--- ci.mk
+++ ci.mk
@@ -20,3 +20,6 @@ ci_release_snapshot:
 ci_release:
 	docker tag ghcr.io/riotkit-org/pgbr:latest-pg${POSTGRES_VERSION} ghcr.io/riotkit-org/pgbr:$${GITHUB_REF##*/}-pg${POSTGRES_VERSION}
 	docker push ghcr.io/riotkit-org/pgbr:$${GITHUB_REF##*/}-pg${POSTGRES_VERSION}
+
+ci_rename_release_binary:
+	cp .build/pgbr .build/pgbr-linux-amd64-glibc-$${GITHUB_REF##*/}-pg${POSTGRES_VERSION}
